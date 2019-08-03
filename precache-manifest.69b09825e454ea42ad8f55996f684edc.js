0757924464d94a472cdb68fbdf17fd532cd79cfc
caiomagri <kaioeduardofm@gmail.com>
2019-08-03 11:42:37 -0300

    Updates

---
+++ precache-manifest.69b09825e454ea42ad8f55996f684edc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d0e50eceef0c6ae74eb715bd86eb99",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "89447cc2e823864d10f4",
+    "url": "/monster-rolodex/static/css/main.274a55a7.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "89447cc2e823864d10f4",
+    "url": "/monster-rolodex/static/js/main.7c7eebbd.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
